45da114b6248e4b85dd9f48565f821051cfdf486
Michael Oliver <jmichaeloliver12@gmail.com>
2016-10-17 15:54:44 -0600

    Before Deployment

--- cakemail.js
+++ cakemail.js
@@ -1,1 +1,5 @@
-//test
+angular.module('cakeMail', [])
+
+.controller('cakeController', function() {
+
+});
--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-console.log('server');
-//text for a test
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('cake-mail');
+});
+
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
+})
