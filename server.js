03a7c5c72d6e0b5fc3ca7808e675ed92b7a4b685
Michael Oliver <jmichaeloliver12@gmail.com>
2016-10-17 17:46:34 -0600

    running on local server

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
-//var path = require('path');
+var path = require('path');
 
 var app = express();
 
+app.set('port', 3000);
 
-app.use(express.static('index.html'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.sendFile('index.html');
-})
-
-// app.get('/', function(req, res) {
-//   res.use(express.static('index.html'));
-// });
-
-app.listen(3000, function() {
+app.listen(app.get('port'), function() {
   console.log('Server listening on port 3000');
 })
