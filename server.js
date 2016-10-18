201c64dd91f8f5b932b6c2b082d3e5d8366ece9e
Michael Oliver <jmichaeloliver12@gmail.com>
2016-10-17 20:34:51 -0600

    heroku local working

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var path = require('path');
 
 var app = express();
 
-app.set('port', 3000);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
-  console.log('Server listening on port 3000');
-})
+  console.log('Server running on port', app.get('port'));
+});
