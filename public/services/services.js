68a0f4210c70c559939bebb684f32f041ece46c9
Michael Oliver <jmichaeloliver12@gmail.com>
2016-10-18 12:49:26 -0600

    service built

---
+++ public/services/services.js
@@ -0,0 +1,29 @@
+angular.module('cakeMail.services', [])
+
+.factory('Orders', function($http) {
+
+  var getOrders = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/orders'
+    })
+    .then(function(resp) {
+      return resp.data;
+    });
+  };
+
+  var addOrder = function(order) {
+    return $http({
+      method: 'POST',
+      url: '/api/orders',
+      data: order
+    })
+    .then(function(resp) {
+      return resp.data;
+    });
+  };
+  return {
+    getOrders: getOrders,
+    addOrder: addOrder
+  };
+})
