746c35b69f2e01b36732f7c051cfa0ebd962bc79
Michael Oliver <jmichaeloliver12@gmail.com>
2016-10-18 14:44:56 -0600

    html files updated

--- public/tasters/tasters.js
+++ public/tasters/tasters.js
@@ -1,43 +1,59 @@
 //tasters.js
+
 var orders = angular.module('cakeMail.tasters', [])
 
-// refactor once db is added
-// .controller('TastersController', function ($scope, Tasters) {
-//   $scope.name = '';
-//   $scope.addLine1 = '';
-//   $scope.addLine2 = '';
-//   $scope.city = '';
-//   $scope.state = '';
-//   $scope.zip = '';
+.controller('TastersController', function ($scope, Tasters, $http) {
+  $http({
+      method: 'POST',
+      url: '/api/orders',
+      data: JSON.stringify({Hello: "Hi"})
+    })
+  $scope.order = {};
 
-//   $scope.addOrder = function() {
+//refactor to be properties of order?? i.e. $scope.order.name
+  // $scope.name = '';
+  // $scope.addLine1 = '';
+  // $scope.addLine2 = '';
+  // $scope.city = '';
+  // $scope.state = '';
+  // $scope.zip = '';
 
-//   }
+  $scope.addOrder = function() {
+    Orders.addOrder($scope.order)
+    //could use .then location path to reroute to thank you, order has been placed
+  };
+
+});
+
+
+
+
+
+
+// .controller('TastersController', function () {
+
+//   var order = this;
+
+//   order.name = '';
+//   order.addLine1 = '';
+//   order.addLine2 = '';
+//   order.city = '';
+//   order.state = '';
+//   order.zip = '';
+
+//   order.orders = [];
+
+//   order.addOrder = function() {
+//     order.orders.push({
+//       name: order.name,
+//       address: order.addLine1 + order.addLine2,
+//       city: order.city,
+//       state: order.state,
+//       zip: order.zip
+//     });
+//   };
 
 // });
 
 
-.controller('TastersController', function () {
 
-  var order = this;
-
-  order.name = '';
-  order.addLine1 = '';
-  order.addLine2 = '';
-  order.city = '';
-  order.state = '';
-  order.zip = '';
-
-  order.orders = [];
-
-  order.addOrder = function() {
-    order.orders.push({
-      name: order.name,
-      address: order.addLine1 + order.addLine2,
-      city: order.city,
-      state: order.state,
-      zip: order.zip
-    });
-  };
-
-});
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var path = require('path');
+var orders = require('./Models/Orders/ordersModel');
+var bodyParser = require('body-parser');
 
-var dbURI = process.env.MONGODB_URI || 'mongosdb://localhost/cakemail';
+var dbURI = process.env.MONGODB_URI || 'mongodb://localhost/cakemail';
 
 mongoose.connect(dbURI);
 
 var app = express();
+
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,3 +19,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(app.get('port'), function() {
   console.log('Server running on port', app.get('port'));
 });
+
+app.get('/api/orders', function(req, res){
+
+})
+
+app.post('/api/orders', function(req, res) {
+  console.log(req.body);
+})
